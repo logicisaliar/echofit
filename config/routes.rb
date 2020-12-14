8bfb4fee467ec80c5b57c535dfd6ae9ef82ee217
Jash Shah <jash.shah@live.com>
2020-12-14 15:37:52 +0530

    Wedding site

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get '/pages/askthem', to: 'pages#askthem'
+  get '/pages/askher', to: 'pages#askher'
+  get '/pages/askhim', to: 'pages#askhim'
+  get '/pages/stalkus', to: 'pages#stalkus'
+  get '/pages/goldhana', to: 'pages#goldhana'
+  get '/pages/wedding', to: 'pages#wedding'
+  get '/pages/party', to: 'pages#party'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
